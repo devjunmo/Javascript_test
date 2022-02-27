304228102dd4065885f3b8273861eb8b9b31b1f5
yajumo93 <yajumo93@gmail.com>
2022-02-27 23:06:58 +0900

    .

---
+++ js_test/test.js
@@ -0,0 +1,68 @@
+// 출처: 유튜브 코딩앙마
+
+var test = 'Hello World';
+
+console.log(test);
+
+
+let name = 'Mike'; // 변할수 있는 변수 앞에 let 붙임
+const NAME = 'Park'; // 상수
+
+name = 'Kim'
+console.log(name)
+
+// NAME = 'Ko'
+// console.log(NAME)
+
+age = 30;
+
+// // alert(name)
+// console.log(name)
+
+
+const NAME3 = 'LEE'
+
+back_tick = `My name is ${NAME3}!!!` // fstring 느낌으로 됨
+console.log(back_tick)
+
+
+const age2 = 30;
+const PI = 3.14;
+
+const my_sum = 300+100
+console.log(my_sum)
+
+
+const my_true = true;
+const my_false = false;
+
+console.log(my_true == true);
+
+
+const my_null = null;
+const my_undef = undefined;
+
+console.log(my_undef);
+
+console.log(typeof null);
+console.log(typeof 3); // number
+console.log(typeof PI); // number
+console.log(typeof 'string');
+console.log(typeof true);
+console.log(typeof undefined);
+
+a = '나는'
+b = '사람'
+c = '2'
+console.log(a+b+c)
+
+
+
+// 클라이언트 사이트 web api임. 여긴 node.js server라 인식못함
+// alert 알려줌 / prompt 입력받음 / confirm 확인받음
+
+// alert('hello alert')
+// prompt()
+// confirm()
+
+
